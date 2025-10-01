e7383fafcf1efcad6ca16d86cd2b279b631724a5
Daza Cano Lara <dazakno43@gmail.com>
2025-10-01 14:40:58 -0600

    script: Implementar función para mostrar todos los productos

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-function renderProduct(product) {
-  return `
-    <div class="product-card">
-      <img src="${product.image}" alt="${product.name}">
-      <h2 class="product-name">${product.name}</h2>
-      <p class="product-price">$${product.price}</p>
-    </div>
-  `;
+const products = []; 
+
+function displayProducts() {
+  const container = document.getElementById("product-container");
+  container.innerHTML = "";
+
+  if (products.length === 0) {
+    container.innerHTML = "<p>Aún no hay panes registrados</p>";
+    return;
+  }
+
+  products.forEach(product => {
+    container.innerHTML += renderProduct(product);
+  });
 }
