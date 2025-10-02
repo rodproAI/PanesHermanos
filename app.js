2ef51893c62ecf4168d22392c4ec12526666cece
LarryIA <dave24419494@gmail.com>
2025-10-02 14:40:36 -0600

    agregar botón de editar a las tarjetas de producto

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 function renderProduct(product) {
   return `
-    <div class="product-card">
+    <div class="product-card" data-id="${product.id}">
       <img src="${product.image}" alt="${product.name}">
       <h2 class="product-name">${product.name}</h2>
       <p class="product-price">$${product.price}</p>
+      
+      <button class="edit-btn">Editar</button>
     </div>
   `;
 }
