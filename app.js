76d5ddfbe16956fe6c0ba1815369388c62fe915b
LarryIA <dave24419494@gmail.com>
2025-10-02 17:47:23 -0600

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function renderProduct(product) {
 }
 
 
-// Lógica principal que se ejecuta al cargar la página
+// Lógica principal que se ejecuta al cargar la págin
 document.addEventListener('DOMContentLoaded', () => {
     const formulario = document.querySelector('#form-agregar-producto');
 
