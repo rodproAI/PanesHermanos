b700f6d0b22f4eee6f3c3825187e9e03349e0fc3
Cesar Cruz <cesc.10.cruz@gmail.com>
2025-10-02 18:41:57 -0600

    docs: añade comentario explicativo al condicional del formulario

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function renderProduct(product) {
 
 
 ddocument.addEventListener('DOMContentLoaded', () => {
-    /*  LÓGICA PARA EL FORMULARIO DE AGREGAR PRODUCTO  */
+    /*  FORMULARIO DE AGREGAR PRODUCTO  */
     /* TAREA 6 */
 
     const formulario = document.querySelector('#form-agregar-producto');
