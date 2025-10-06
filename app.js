626c67e3fb07d1b3e7075fe41f1ae46c416dd449
LarryIA <dave24419494@gmail.com>
2025-10-05 21:17:27 -0600

    commit 6 UPDATE 05 - añadir un if para veirificar que el producto fue encontrado.

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ document.addEventListener('DOMContentLoaded', () => {
    if (formEditar) {
        const productId = localStorage.getItem('editProductId');
        const productoAEditar = baseDeDatosProductos.find(producto => producto.id == productId);
+       if (productoAEditar) {
+       }
    }
 
 
